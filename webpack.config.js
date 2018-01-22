f6a7c2559f1b561043f9cee9c5131a41246cf57c
Kyle Morton <kylemortondevelopment@gmail.com>
2018-01-21 19:44:01 -0600

    added babel, css loaders in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,20 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.join(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, //find file that matches this expression
+                use: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [ //example of an array of loaders for given expression
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
     }
 }
